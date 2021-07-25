38989c20388e7d5d063f9b5d739d385427f797b2
Ashish Singh <ashish.k.singh1001@gmail.com>
2021-07-26 01:26:02 +0530

    added tabs for navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:meals/screens/meal_detail_screen.dart';
+import './screens/meal_detail_screen.dart';
+import './screens/tabs_screen.dart';
 import './screens/categories_meals_screen.dart';
 import './screens/categories_screen.dart';
 
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
       ),
       // home: CategoriesScreen(),
       routes: {
-        '/': (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
